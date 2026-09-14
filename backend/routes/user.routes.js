126d927e9781e45a5c77592763ce6d7198eef8b3
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-14 18:18:59 +0530

    Add temporary Multer file upload test route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -6,9 +6,11 @@ import {
     getMe,
     getUserProfile,
     followUser,
-    unfollowUser
+    unfollowUser,
+    testFileUpload
 } from "../controllers/user.controllers.js";
 import isAuthenticated from "../middlewares/authMiddleware.js";
+import upload from "../middlewares/upload.middleware.js";
 
 const userRoutes = express.Router();
 
@@ -17,6 +19,14 @@ userRoutes.post("/login", loginUser);
 userRoutes.post("/logout", isAuthenticated, logoutUser);
 userRoutes.get("/me", isAuthenticated, getMe);
 userRoutes.get("/profile/:username", isAuthenticated, getUserProfile);
+
+// Temporary Multer test route
+userRoutes.post(
+    "/test-upload",
+    isAuthenticated,
+    upload.single("profileImage"),
+    testFileUpload
+);
 
 // Following and followers
 userRoutes.post("/:id/follow", isAuthenticated, followUser);
