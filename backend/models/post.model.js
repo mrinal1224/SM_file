51dd3d58198b2103426b2a867402d561752f1626
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-20 13:58:12 +0530

    Add Post model

---
+++ backend/models/post.model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+    {
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        caption: {
+            type: String,
+            trim: true,
+            maxlength: 500
+        },
+        image: {
+            type: String
+        }
+    },
+    { timestamps: true }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+export default Post;
