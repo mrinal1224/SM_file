9798ba3faa41985464cf5be7f0e474ee1a9bd59f
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-20 14:08:27 +0530

    Mount reel routes

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,7 @@ import { fileURLToPath } from "url";
 
 import userRoutes from "./routes/user.routes.js";
 import postRoutes from "./routes/post.routes.js";
+import reelRoutes from "./routes/reel.routes.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 
 const __filename = fileURLToPath(import.meta.url);
@@ -53,6 +54,7 @@ app.use(cookieParser());
 
 app.use("/users", userRoutes);
 app.use("/posts", postRoutes);
+app.use("/reels", reelRoutes);
 
 app.use(errorMiddleware);
 
