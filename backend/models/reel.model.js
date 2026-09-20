697455ce3afee70142ed228107e14545ae3dc6e0
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-20 14:08:18 +0530

    Add Reel model

---
+++ backend/models/reel.model.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const reelSchema = new mongoose.Schema(
+    {
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        caption: {
+            type: String,
+            trim: true,
+            maxlength: 300
+        },
+        video: {
+            type: String,
+            required: true
+        }
+    },
+    { timestamps: true }
+);
+
+const Reel = mongoose.model("Reel", reelSchema);
+
+export default Reel;
