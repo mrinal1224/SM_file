fb856b03f3860e402aefa4dcfb241ceb498548c1
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-20 13:58:15 +0530

    Add post controllers

---
+++ backend/controllers/post.controllers.js
@@ -0,0 +1,63 @@
+import Post from "../models/post.model.js";
+import User from "../models/user.model.js";
+import uploadToCloudinary from "../utils/uploadToCloudinary.js";
+
+export const createPost = async (req, res, next) => {
+    try {
+        const { caption } = req.body;
+
+        if (!caption?.trim() && !req.file) {
+            return res.status(400).json({
+                message: "Add a caption or upload an image"
+            });
+        }
+
+        if (caption && caption.trim().length > 500) {
+            return res.status(400).json({
+                message: "Caption cannot exceed 500 characters"
+            });
+        }
+
+        let image;
+
+        if (req.file) {
+            const uploadedImage = await uploadToCloudinary(req.file.buffer);
+            image = uploadedImage.secure_url;
+        }
+
+        const post = await Post.create({
+            author: req.user._id,
+            caption: caption?.trim() || "",
+            image
+        });
+
+        await User.findByIdAndUpdate(req.user._id, {
+            $push: { posts: post._id }
+        });
+
+        const populatedPost = await Post.findById(post._id)
+            .populate("author", "name username profileImage");
+
+        return res.status(201).json({
+            message: "Post created successfully",
+            post: populatedPost
+        });
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const getFeed = async (req, res, next) => {
+    try {
+        const posts = await Post.find()
+            .sort({ createdAt: -1 })
+            .populate("author", "name username profileImage");
+
+        return res.status(200).json({
+            message: "Feed fetched successfully",
+            posts
+        });
+    } catch (error) {
+        next(error);
+    }
+};
