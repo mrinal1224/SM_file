a995c7d1bcd337290a1107c318a01259110e2125
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-21 17:25:13 +0530

    feat: add likes to posts

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -14,7 +14,13 @@ const postSchema = new mongoose.Schema(
         },
         image: {
             type: String
-        }
+        },
+        likes: [
+            {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User"
+            }
+        ]
     },
     { timestamps: true }
 );
