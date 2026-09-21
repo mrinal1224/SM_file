a5a7b77630146dc2095e0e8343a7f22b2be3b304
Mrinal Bhattacharya <iammrinalkc@gmail.com>
2026-09-21 17:25:43 +0530

    feat: add comment model

---
+++ backend/models/comment.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const commentSchema = new mongoose.Schema(
+    {
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true
+        },
+        text: {
+            type: String,
+            trim: true,
+            required: true,
+            maxlength: 500
+        },
+        post: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Post"
+        },
+        reel: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Reel"
+        }
+    },
+    { timestamps: true }
+);
+
+const Comment = mongoose.model("Comment", commentSchema);
+
+export default Comment;
